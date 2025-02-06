e60fd0e71bcc5516e6785a481d90e1c18874188a
JiSeon <jsseong0416@gmail.com>
2025-02-06 14:54:52 +0900

    saleDto, saleDao 다시 제작

--- middle/src/main/java/test/dao/Com1SaleDao.java
+++ middle/src/main/java/test/dao/Com1SaleDao.java
@@ -1,8 +1,10 @@
 package test.dao;
 
+import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,20 +13,21 @@ import test.dto.Com1SaleDto;
 import test.util.DbcpBean;
 
 public class Com1SaleDao {
-	
+
 	private static Com1SaleDao dao;
-	
+
 	static {
-		dao=new Com1SaleDao();
+		dao = new Com1SaleDao();
 	}
-	
-	private Com1SaleDao() {}
-	
+
+	private Com1SaleDao() {
+	}
+
 	public static Com1SaleDao getInstance() {
 		return dao;
 	}
-	
-	//추가
+
+	// 추가
 	public boolean insert(Com1SaleDto dto) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -33,14 +36,14 @@ public class Com1SaleDao {
 			conn = new DbcpBean().getConn();
 			String sql = """
 					insert into test_com1_sales
-					(id, storenum, salemonth, monthlysal, created_at)
-					values(test_saleid_seq.nextval, ? , ? ,? ,SYSDATE)
+					(saleDate, storeNum, dailySales)
+					values(?,?,?)
 					""";
 			pstmt = conn.prepareStatement(sql);
 			// ? 에 값을 여기서 바인딩한다.
-			pstmt.setInt(1, dto.getStorenum());
-			pstmt.setString(2, dto.getSalemonth());
-			pstmt.setInt(3, dto.getMonthlysal());
+			pstmt.setString(1, dto.getSaleDate());
+			pstmt.setInt(2, dto.getStoreNum());
+			pstmt.setInt(3, dto.getDailySales());
 			// sql 문을 실행하고 변화된 row 의 개수를 리턴받기
 			rowCount = pstmt.executeUpdate();
 		} catch (Exception e) {
@@ -60,42 +63,45 @@ public class Com1SaleDao {
 			return false;
 		}
 	}
-	
-	//삭제
-	//?호점의 ?월 매출 삭제
-	public boolean delete(int storenum, String salemonth ) {
+
+	// 삭제
+	// ?호점의 ?일 매출 삭제
+	public boolean delete(String saleDate, int storeNum) {
 		Connection conn = null;
-        PreparedStatement pstmt = null;
-        int rowCount = 0;
-        try {
-            conn = new DbcpBean().getConn();
-            String sql = """
-                delete from test_com1_sales
-                WHERE storenum=? AND salemonth=?
-            """;
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setInt(1, storenum);
-            pstmt.setString(2, salemonth );
-            rowCount = pstmt.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (pstmt != null) pstmt.close();
-                if (conn != null) conn.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        if (rowCount > 0) {
+		PreparedStatement pstmt = null;
+		int rowCount = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = """
+					    delete from test_com1_sales
+					    WHERE storeNum=? and saleDate=?
+					""";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(2, storeNum);
+			pstmt.setString(1, saleDate);
+
+			rowCount = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (rowCount > 0) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
-	//수정
-	//?호점의 ?월 매출 수정
+
+	// 수정
+	// ?호점의 ?월 매출 수정
 	public boolean update(Com1SaleDto dto) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -104,14 +110,15 @@ public class Com1SaleDao {
 			conn = new DbcpBean().getConn();
 			String sql = """
 					update test_com1_sales
-					set monthlysal=?
-					where storenum=? and  salemonth=?
+					set dailySales=?
+					where storeNum=? and saleDate=?
 					""";
 			pstmt = conn.prepareStatement(sql);
 			// ? 에 값을 여기서 바인딩한다.
-			pstmt.setInt(1, dto.getMonthlysal());
-			pstmt.setInt(2, dto.getStorenum());
-			pstmt.setString(3, dto.getSalemonth());
+			pstmt.setInt(1, dto.getDailySales());
+			pstmt.setInt(2, dto.getStoreNum());
+			pstmt.setString(3, dto.getSaleDate());
+
 			// sql 문을 실행하고 변화된 row 의 개수를 리턴받기
 			rowCount = pstmt.executeUpdate();
 		} catch (Exception e) {
@@ -131,59 +138,229 @@ public class Com1SaleDao {
 			return false;
 		}
 	}
-	
-	//하나의 데이터 얻어오기
-	//?호점 하나의 달 데이터 얻어오기
-	public Com1SaleDto getData(int storenum, String salemonth) {
-	    // Dto 객체 선언
-	    Com1SaleDto dto = null;
 
-	    Connection conn = null;
-	    PreparedStatement pstmt = null;
-	    ResultSet rs = null;
-	    try {
-	        // Connection Pool에서 Connection 객체 가져오기
-	        conn = new DbcpBean().getConn();
-	        // 실행할 SQL 문 작성
-	        String sql = """
-	            SELECT id, storenum, salemonth, monthlysal, created_at,
-	                   monthlysal - LAG(monthlysal, 1, 0) OVER (PARTITION BY storenum ORDER BY TO_NUMBER(salemonth)) AS diff
-	            FROM test_com1_sales
-	            WHERE storenum=? AND salemonth=?
-	        """;
-	        pstmt = conn.prepareStatement(sql);
-	        // ? 에 값 바인딩
-	        pstmt.setInt(1, storenum);
-	        pstmt.setString(2, salemonth);
-	        // SQL 문 실행하고 결과를 ResultSet 객체로 받기
-	        rs = pstmt.executeQuery();
-	        if (rs.next()) {
-	            // Dto 객체 생성 후 값 설정
-	            dto = new Com1SaleDto();
-	            dto.setId(rs.getInt("id"));
-	            dto.setStorenum(rs.getInt("storenum"));
-	            dto.setSalemonth(rs.getString("salemonth"));
-	            dto.setMonthlysal(rs.getInt("monthlysal"));
-	            dto.setCreated_at(rs.getString("created_at"));
-	            dto.setDiff(rs.getInt("diff")); 
-	        }
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    } finally {
-	        try {
-	            if (rs != null) rs.close();
-	            if (pstmt != null) pstmt.close();
-	            if (conn != null) conn.close();
-	        } catch (Exception e) {
-	        }
-	    }
-	    return dto; 
+	// 하나의 데이터 얻어오기
+	// ?호점 하나의 일 매출 데이터 얻어오기
+	public Com1SaleDto getData(String saleDate, int storeNum) {
+		// Dto 객체 선언
+		Com1SaleDto dto = null;
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			// Connection Pool에서 Connection 객체 가져오기
+			conn = new DbcpBean().getConn();
+			// 실행할 SQL 문 작성
+			String sql = """
+					    SELECT saleDate, storeNum, dailySales
+					    FROM test_com1_sales
+					    WHERE saleDate=? and storeNum=?
+					""";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 값 바인딩
+			pstmt.setString(1, saleDate);
+			pstmt.setInt(2, storeNum);
+
+			// SQL 문 실행하고 결과를 ResultSet 객체로 받기
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				// Dto 객체 생성 후 값 설정
+				dto = new Com1SaleDto();
+				dto.setSaleDate(rs.getString("saleDate"));
+				dto.setStoreNum(rs.getInt("storeNum"));
+				dto.setDailySales(rs.getInt("dailySales"));
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return dto;
+	}
+
+	// list 가져오기
+	// ?호점 ?월 매출정보 가져오기
+	public List<Com1SaleDto> getListStoreMonth(int storeNum, int year, int month) {
+		List<Com1SaleDto> list = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = """
+					SELECT YEAR(salesDate) AS year, MONTH(salesDate) AS month, storeNum,
+					       SUM(dailySales) AS monthlySales
+					FROM test_com1_sales
+					WHERE storeNum=? AND YEAR(salesDate)=? AND MONTH(salesDate)=?
+					GROUP BY year, month, storeNum;
+					     """;
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, storeNum);
+			pstmt.setInt(2, year);
+			pstmt.setInt(3, month);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				Com1SaleDto dto = new Com1SaleDto();
+				dto.setDailySales(rs.getInt("dailySales"));
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return list;
+	}
+
+	// ?호점 ?연 매출정보 가져오기
+	public List<Com1SaleDto> getListStoreYear(int storeNum, int year) {
+		List<Com1SaleDto> list = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = """
+					SELECT YEAR(salesDate) AS year, storeNum,
+					       SUM(dailySales) AS yearlySales
+					FROM test_com1_sales
+					WHERE storeNum=? AND YEAR(salesDate)=?
+					GROUP BY year, storeNum;
+					     """;
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, storeNum);
+			pstmt.setInt(2, year);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				Com1SaleDto dto = new Com1SaleDto();
+				dto.setDailySales(rs.getInt("dailySales"));
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return list;
+	}
+
+	// 전체매장 ?월 매출정보 가져오기
+	public List<Com1SaleDto> getListMonth(int year, int month) {
+		List<Com1SaleDto> list = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = """
+					SELECT YEAR(salesDate) AS year, MONTH(salesDate) AS month,
+					       SUM(dailySales) AS monthlySales
+					FROM test_com1_sales
+					WHERE YEAR(salesDate)=? AND MONTH(salesDate)=?
+					GROUP BY year, month;
+					     """;
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, year);
+			pstmt.setInt(2, month);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				Com1SaleDto dto = new Com1SaleDto();
+				dto.setDailySales(rs.getInt("dailySales"));
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return list;
+	}
+
+	// 전체매장 ?연 매출정보 가져오기
+	public List<Com1SaleDto> getListYear(int year) {
+		List<Com1SaleDto> list = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = """
+					SELECT YEAR(salesDate) AS year,
+					       SUM(dailySales) AS yearlySales
+					FROM test_com1_sales
+					WHERE YEAR(salesDate)=?
+					GROUP BY year, storeNum;
+					     """;
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, year);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				Com1SaleDto dto = new Com1SaleDto();
+				dto.setDailySales(rs.getInt("dailySales"));
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return list;
 	}
 
 	
-	//list 가져오기
-	//?호점 매출정보 가져오기
-	public List<Com1SaleDto> getList(int storenum) {
+	public List<Com1SaleDto> getListAll() {
 	    List<Com1SaleDto> list = new ArrayList<>();
 	    Connection conn = null;
 	    PreparedStatement pstmt = null;
@@ -192,22 +369,18 @@ public class Com1SaleDao {
 	    try {
 	        conn = new DbcpBean().getConn();
 	        String sql = """
-	            SELECT salemonth, 
-	                   monthlysal, 
-	                   monthlysal - LAG(monthlysal, 1, 0) OVER (PARTITION BY storenum ORDER BY TO_NUMBER(salemonth)) AS diff
-	            FROM test_com1_sales
-	            WHERE storenum = ?
-	            ORDER BY salemonth
+				SELECT *from test_com1_sales
 	        """;
 	        pstmt = conn.prepareStatement(sql);
-	        pstmt.setInt(1, storenum);
+	      
+	  
 	        rs = pstmt.executeQuery();
-
 	        while (rs.next()) {
 	            Com1SaleDto dto = new Com1SaleDto();
-	            dto.setSalemonth(rs.getString("salemonth"));
-	            dto.setMonthlysal(rs.getInt("monthlysal"));
-	            dto.setDiff(rs.getInt("diff")); // 차이값 추가
+	            dto.setSaleDate(rs.getString("salesDate"));
+	            dto.setStoreNum(rs.getInt("storeNum"));
+	            dto.setDailySales(rs.getInt("dailySales"));
+	            
 	            list.add(dto);
 	        }
 	    } catch (Exception e) {
